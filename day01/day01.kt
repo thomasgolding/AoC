2ebfb93b69c8cebcd3fcb2d0866e7adf340f49bb
Thomas Golding <thomas.golding@gmail.com>
2020-12-05 23:22:43 +0100

    day 1 part 2

--- day01/day01.kt
+++ day01/day01.kt
@@ -9,20 +9,41 @@ fun main() {
     numberstream.bufferedReader().forEachLine {it -> numberlist.add(it.toInt())}
     
     val imax: Int = numberlist.size
-    var a: Int=-1
-    var b: Int=-1
+    var a: Int=0
+    var b: Int=0
+
+    var a2: Int=0
+    var b2: Int=0
+    var c2: Int=0
 
     for ((i, num1) in numberlist.withIndex()) {
-        for (num2 in numberlist.slice(i..imax-1)) {
+  
+        for ((i2,num2) in numberlist.slice(i..imax-1).withIndex()) {
             if (num1+num2 == 2020) {
-                a=num1
-                b=num2
+                a = num1
+                b = num2
+            }
+            for (num3 in numberlist.slice(i+i2..imax-1)) {
+                
+                if (num1+num2+num3 == 2020) {
+                    a2 = num1
+                    b2 = num2
+                    c2 = num3
+                }
+
             }
         }        
     }
 
+    println("Part 1")
     println(a*b)
-    
+
+    println()
+    println("Part 2")
+    println(a2*b2*c2)
+
+
+
 
 }
 
